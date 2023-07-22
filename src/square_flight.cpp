3b2ba3c06520b0bd6cbd6bd9cb8a9dfce45832e2
TalesLimaOliveira <104475181+TalesLimaOliveira@users.noreply.github.com>
2023-07-22 16:19:05 -0300

    Update square_flight.cpp

--- src/square_flight.cpp
+++ src/square_flight.cpp
@@ -2,16 +2,26 @@
 
 void set_waypoints()
 {
+    // Define a posição x do ponto de destino
+    double tam = 3;  // Coordenada x do ponto de destino
+    double altitute = 1.7;
+    
     // Define a lista de waypoints
     std::vector<gnc_api_waypoint> waypointList = {
         // x, y, z, psi
-        {0, 0, 2, 0},      // Waypoint 1: x=0, y=0, z=2, psi=0
-        {2, 0, 2, -90},    // Waypoint 2: x=2, y=0, z=2, psi=-90
-        {2, 2, 2, 0},      // Waypoint 3: x=2, y=2, z=2, psi=0
-        {0, 2, 2, 90},     // Waypoint 4: x=0, y=2, z=2, psi=90
-        {0, 0, 2, 180},    // Waypoint 5: x=0, y=0, z=2, psi=180
-        {0, 0, 2, 0}       // Waypoint 6: x=0, y=0, z=2, psi=0
+        {0, 0, altitute, 0},      // Waypoint 1: x=0, y=0, z=2, psi=0
+        {2, 0, altitute, -90},    // Waypoint 2: x=2, y=0, z=2, psi=-90
+        {2, 2, altitute, 0},      // Waypoint 3: x=2, y=2, z=2, psi=0
+        {0, 2, altitute, 90},     // Waypoint 4: x=0, y=2, z=2, psi=90
+        {0, 0, altitute, 180},    // Waypoint 5: x=0, y=0, z=2, psi=180
+        {0, 0, altitute, 0}       // Waypoint 6: x=0, y=0, z=2, psi=0
     };
+
+    // Velocidade linear desejada em m/s
+    set_speed(0.1);
+
+    // Realiza a decolagem com uma altitude de 3 metros
+    takeoff(altitute);
 
     int counter = 0;
     while (ros::ok())
@@ -49,12 +59,6 @@ void takeoff_and_wp()
 
     // Inicializa o frame de referência local
     initialize_local_frame();
-
-    // Velocidade linear desejada em m/s
-    set_speed(0.3);
-
-    // Realiza a decolagem com uma altitude de 3 metros
-    takeoff(2);
 
     // Define e percorre os waypoints
     set_waypoints();
